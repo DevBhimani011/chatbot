01611d2630e7ffdb626a385c69729e5c79c30a3a
DevBhimani <dev.bhimani@silvertouch.com>
2026-01-28 10:24:31 +0530

    Add requirements.txt

--- backend/run.py
+++ backend/run.py
@@ -3,4 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run("app.main:app", reload=True)
 
+
+# cloudflared tunnel --url http://localhost:8000
+# cloudflared tunnel --url http://localhost:3000
 # uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
